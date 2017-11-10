5cb9e6a66631ee4e61403d79ed3339713c984368
Vivek Sejpal <vsejpal@gmail.com>
2017-11-10 10:41:13 -0800

    models - first commit - incomplete, need fixup

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 
 # Create your models here.
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """"Represents a 'user profile' in our system"""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+
+    def get_full_name(self):
+        """"Used to get a user's full name"""
+
+        return self.name
+
+    def get_short_name(self):
+        """"Used to get user's short name"""
+
+        return self.name
+
+    def str(self):
+        """" so django knows how to print an object of this class"""
+
+        return self.email
