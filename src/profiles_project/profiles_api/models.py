4bec7afc4de3caddb98f95194a2fcc4db9e9f646
Vivek Sejpal <vsejpal@gmail.com>
2017-11-20 10:56:41 -0800

    correct typo to fix recursion when creating superuser

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -27,7 +27,7 @@ class UserProfileManager(BaseUserManager):
     def create_superuser(self, email, name, password):
         """"Creates a new superuser with given details"""
 
-        user = self.create_superuser(email, name, password)
+        user = self.create_user(email, name, password)
 
         user.is_superuser = True
         user.is_staff = True
